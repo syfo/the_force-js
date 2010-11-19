7a9c766c27e17f3d3cec666b0a921b481db010d3
Ryan Ziegler <ryan@symbolforce.com>
2010-11-19 08:41:20 -0400

    adding swap function

---
+++ src/jquery.swap.js
@@ -0,0 +1,9 @@
+$.fn.swap = function(b) { 
+    b = jQuery(b)[0]; 
+    var a = this[0]; 
+    var t = a.parentNode.insertBefore(document.createTextNode(''), a); 
+    b.parentNode.insertBefore(a, b); 
+    t.parentNode.insertBefore(b, t); 
+    t.parentNode.removeChild(t); 
+    return this; 
+};
